a4ad856a593d73bba89921fdd26a44e610789a05
EC2 Default User <ec2-user@ip-172-31-17-104.us-east-2.compute.internal>
2020-03-27 11:01:35 +0000

    staic→staics, add help about

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'staics/home'
+  get 'staics/help'
+  get 'staics/about'
+
   root 'application#hello'
-  
-  get 'static/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
