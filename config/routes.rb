af138b34836f9a39cf77419b57923ad96f3d383b
EC2 Default User <ec2-user@ip-172-31-17-104.us-east-2.compute.internal>
2020-04-24 00:56:40 +0000

    change titles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'staics/help'
   get 'staics/about'
 
-  root 'application#hello'
+  root 'staics#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/staics_controller_test.rb
+++ test/controllers/staics_controller_test.rb
@@ -1,27 +1,20 @@
 require 'test_helper'
 
 class StaicsControllerTest < ActionDispatch::IntegrationTest
-  
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
 
   test "should get home" do
     get staics_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get staics_help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get staics_about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
   end
 
 end
